7e51f90ed5cbdf3dad04604fdc4121269a1ed081
Shabbir Rayeen <sawone2893@gmail.com>
2026-02-03 09:22:42 +0530

    Adding datatypes and varibales concept.

---
+++ topic1_datatypes_variables/datatype_variables.py
@@ -0,0 +1,17 @@
+# This script demonstrates different data types and variable assignments in Python.
+num=10 # Integer variable
+name='Alice' # String variable
+is_active=True # Boolean variable
+pi=3.14 # Float variable
+fruits=['apple', 'banana', 'cherry'] # List variable
+person={'name': 'Bob', 'age': 25} # Dictionary variable
+coordinates=(10.0, 20.0)    # Tuple variable
+unique_id=None  # NoneType variable
+print("Integer:", num)
+print("String:", name) 
+print("Boolean:", is_active)
+print("Float:", pi)
+print("List:", fruits)
+print("Dictionary:", person)
+print("Tuple:", coordinates)
+print("NoneType:", unique_id)
